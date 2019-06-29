2b0abee3972e00ecf03dfe52e6d35f4941fcaf7c
linjj93 <linjj93@hotmail.com>
2019-06-29 15:53:08 +0800

    Main app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require("./db");
 const express = require("express");
 const app = express();
 const pokemonRouter = require("./routes/pokemon.route");
-
 app.use(express.json());
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/pokemon", pokemonRouter);
 
 // Error Handler
