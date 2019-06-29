9e1af4a19e0f1e3da5c79a0bfef10a0152d8352f
linjj93 <linjj93@hotmail.com>
2019-06-29 15:52:20 +0800

    Add pokemon route and tests

--- routes/pokemon.route.js
+++ routes/pokemon.route.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const router = express.Router();
+require("../db");
 require("../pokemon.model");
 const mongoose = require("mongoose");
-const Pokemon = mongoose.model("pokemon");
+const Pokemon = mongoose.model("Pokemon");
+const flatten = require("flat");
+
+// router.use(express.json());
 
 router.get("/", async (req, res) => {
   const foundPokemon = await Pokemon.find();
   res.status(200).json(foundPokemon);
+});
+
+router.get("/:id", async (req, res) => {
+  const foundPokemon = await Pokemon.findOne({ id: req.params.id });
+  res.status(200).json(foundPokemon);
+});
+
+router.post("/", async (req, res) => {
+  const newPokemon = new Pokemon(req.body);
+  await newPokemon.save();
+  res.status(201).json(newPokemon);
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const fieldsToUpdate = flatten(req.body);
+    await Pokemon.findOneAndUpdate({ id }, fieldsToUpdate);
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedPokemon = await Pokemon.findOne({ id: req.params.id });
+  await Pokemon.deleteOne({ id: req.params.id });
+  res.status(200).json(deletedPokemon);
 });
 
 module.exports = router;
--- tests/pokemon.route.test.js
+++ tests/pokemon.route.test.js
@@ -43,5 +43,63 @@ describe("Pokemon", () => {
       const response = await request(app).get("/pokemon");
       expect(response.body).toMatchObject(pokemonData);
     });
+
+    it("POST / should add new Pokemon", async () => {
+      const requestBody = pokemonData[1];
+
+      const response = await request(app)
+        .post("/pokemon")
+        .send(requestBody)
+        .set("Content-Type", "application/json");
+
+      const collection = db.collection("pokemons");
+      expect(response.status).toEqual(201);
+      const foundPokemon = await collection.findOne({ id: requestBody.id });
+      expect(response.body).toMatchObject(requestBody);
+      expect(foundPokemon.id).toEqual(requestBody.id);
+    });
+  });
+
+  describe("/pokemon/:id", () => {
+    it("GET /7 should return Squirtle", async () => {
+      const collection = db.collection("pokemons");
+      await collection.insertMany(pokemonData);
+
+      const response = await request(app).get("/pokemon/7");
+      expect(response.body).toMatchObject(pokemonData[0]);
+    });
+
+    it("DELETE /6 should remove Charizard", async () => {
+      const collection = db.collection("pokemons");
+      await collection.insertMany(pokemonData);
+
+      const deletedPokemon = await collection.findOne({
+        id: 6
+      });
+      const response = await request(app).delete("/pokemon/6");
+      const deletedPokemonRemains = await collection.findOne({
+        id: 6
+      });
+      expect(response.body).toMatchObject(deletedPokemon);
+      expect(response.status).toEqual(200);
+      expect(deletedPokemonRemains).toBeFalsy();
+    });
+
+    it("PUT /10 should update Caterpie", async () => {
+      const collection = db.collection("pokemons");
+      await collection.insertMany(pokemonData);
+      const fieldsToUpdate = {
+        base: {
+          HP: 80
+        }
+      };
+      const response = await request(app)
+        .put("/pokemon/10")
+        .send(fieldsToUpdate)
+        .set("Content-Type", "application/json");
+      expect(response.status).toEqual(200);
+      const updatedPokemon = await collection.findOne({ id: 10 });
+      expect(updatedPokemon.base.HP).toEqual(fieldsToUpdate.base.HP);
+    });
   });
 });
